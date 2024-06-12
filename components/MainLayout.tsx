053e06488d2f418d1730d8fb5509a6ea8c67b0a1
mmd-ghodoosi <mamadakgh@gmail.com>
2024-06-12 08:20:46 +0330

    added Main Layout

---
+++ components/MainLayout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import "aos/dist/aos.css";
+
+import HeaderBox from "./sections/Header-box";
+import HeaderInfo from "./sections/Header-info";
+import AboutMe from "./sections/AboutMe";
+import Experience from "./sections/Experience";
+import Skills from "./sections/Skills";
+import LikesSection from "./sections/Likes-Section";
+import Contacts from "./sections/Contacts";
+
+import { Container } from "@mui/material";
+
+const Content = () => {
+  return (
+    <Container maxWidth="sm">
+      <HeaderBox />
+      <HeaderInfo />
+      <AboutMe />
+      <Experience />
+      <Skills />
+      <LikesSection />
+      <Contacts />
+    </Container>
+  );
+};
+
+export default Content;
